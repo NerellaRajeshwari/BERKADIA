ac16a259de7b6b87f3f1632aa9d05addf67e7411
NerellaRajeshwari <36570159+NerellaRajeshwari@users.noreply.github.com>
2018-11-21 17:09:42 +0530

    Update emp.ts

--- WEEK2/ANGULAR-TYPESCRIPT/SORT TABLE/emp.ts
+++ WEEK2/ANGULAR-TYPESCRIPT/SORT TABLE/emp.ts
@@ -1,1 +1,24 @@
-c
+import {Data} from './datatable';
+
+
+var employee:Data[]=[
+{ename:'ram',eid:2,salary:10000,dept:"A1"},
+{ename:'sita',eid:5,salary:20000,dept:"A2"},
+{ename:'lucky',eid:3,salary:30000,dept:"A3"},
+{ename:'bharath',eid:1,salary:30000,dept:"A3"},
+{ename:'rani',eid:4,salary:20000,dept:"A2"},
+{ename:'vani',eid:6,salary:10000,dept:"A1"}];
+
+
+console.log("BEFORE SORTING THE EMPLOYEES");
+console.table(employee);
+
+console.log("AFTER SORTING THE EMPLOYEES BASED ON EID");
+employee.sort(function(a,b)
+{ 
+  return a.eid-b.eid
+});
+console.table(employee);
+
+output:
+//in this program im displaying the output in command prompt
