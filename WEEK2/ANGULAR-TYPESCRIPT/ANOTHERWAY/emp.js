13fe9e996601c501d35f6016fce64febb83cea0a
NerellaRajeshwari <36570159+NerellaRajeshwari@users.noreply.github.com>
2018-11-21 16:32:52 +0530

    Create emp.js

---
+++ WEEK2/ANGULAR-TYPESCRIPT/ANOTHERWAY/emp.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var employee = [{ eid: 101, ename: "Ram", salary: 10000, dept: "A1" },
+    { eid: 102, ename: "Sita", salary: 20000, dept: "A2" },
+    { eid: 103, ename: "Lucky", salary: 30000, dept: "A3" },
+    { eid: 104, ename: "Bharath", salary: 20000, dept: "A2" },
+    { eid: 105, ename: "Kush", salary: 30000, dept: "A3" },
+    { eid: 106, ename: "Rani", salary: 10000, dept: "A1" },
+    { eid: 107, ename: "Siva", salary: 20000, dept: "A2" },
+    { eid: 108, ename: "Vani", salary: 30000, dept: "A3" },
+];
+console.table(employee);
