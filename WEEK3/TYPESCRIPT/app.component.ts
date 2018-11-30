1e773362e5ca3fc4aea3918383f93952043cf657
NerellaRajeshwari <36570159+NerellaRajeshwari@users.noreply.github.com>
2018-11-30 23:48:16 +0530

    Create app.component.ts

---
+++ WEEK3/TYPESCRIPT/app.component.ts
@@ -0,0 +1,57 @@
+  import { Component, OnInit } from '@angular/core';
+    import { GalleryService } from './gallery.service';
+    @Component({
+      selector: 'app-root',
+      templateUrl: './app.component.html',
+      styleUrls: ['./app.component.css']
+    })
+    export class AppComponent implements OnInit {
+      private galleryData;
+      private fullMovieDetials;
+      private reviews;
+      private similarMovies;
+      constructor(private galleryService: GalleryService) { }
+      ngOnInit() {
+        this.galleryService.giveImageData().subscribe((res) => {
+          this.galleryData = this.addBaseURL(res['results']);
+          console.log(this.galleryData);
+        });
+    
+      }
+      addBaseURL(response) {
+        if (Array.isArray(response)) {
+          response.forEach(element => {
+            if (element.backdrop_path) {
+              element.backdrop_path = 'http://image.tmdb.org/t/p/w300_and_h450_bestv2/' + element.backdrop_path;
+    
+            } else {
+              element.backdrop_path = 'http://image.tmdb.org/t/p/w300_and_h450_bestv2/' + '/xgbeBCjmFpRYHDF7tQ7U98EREWp.jpg';
+            }
+          });
+        } else {
+          response.backdrop_path = 'http://image.tmdb.org/t/p/w1280/' + response.backdrop_path;
+    
+        }
+    
+        return response;
+      }
+    
+      getFullDetialsOfMovie(movieId) {
+        this.galleryService.getDetails(movieId).subscribe((res) => {
+          this.fullMovieDetials = this.addBaseURL(res);
+          console.log(this.fullMovieDetials);
+        });
+      }
+      getReviews(movieId) {
+        this.galleryService.getDetails(movieId).subscribe((res) => {
+          this.reviews = this.addBaseURL(res);
+          console.log(this.reviews);
+        });
+      }
+      getSimilarMovies(movieId) {
+        this.galleryService.getDetails(movieId).subscribe((res) => {
+          this.similarMovies = this.addBaseURL(res);
+          console.log(this.similarMovies);
+        });
+      }
+    }
