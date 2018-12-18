03efc97a890f4f218b00aa5c24c56e3edd130d96
NerellaRajeshwari <36570159+NerellaRajeshwari@users.noreply.github.com>
2018-12-18 15:02:54 +0530

    Create express.urlencoded.js

---
+++ WEEK5/NODE-JS/EXPRESS-METHODS/express/express.urlencoded.js
@@ -0,0 +1,24 @@
+code:
+
+//Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
+//This parser accepts only UTF-8 encoding of the body and supports automatic inflation of gzip and deflate encodings
+
+
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+
+// it will support json encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/api/users', function(req, res) {
+  res.send("Hi "+req.body.firstname + ' ' + req.body.lastname +' ' + "welcome to berkadia");
+});
+
+// creating a dynamic port to have a secure connecction 
+const port=process.env.PORT || 3000;
+app.listen(port, ()=> console.log(`listening to ${port}....`));
+
+
+output:
+
