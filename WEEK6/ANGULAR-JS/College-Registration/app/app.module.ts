5916b2c02f3fe763ae468310284598b4ada774e8
NerellaRajeshwari <36570159+NerellaRajeshwari@users.noreply.github.com>
2019-01-14 19:15:36 +0530

    Create app.module.ts

---
+++ WEEK6/ANGULAR-JS/College-Registration/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { StudentListComponent } from './student/student-list.component';
+import { CreateStudentComponent } from './student/create-student.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StudentService} from './student/student.service';
+import { HomeComponent } from './home/home.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentListComponent,
+    CreateStudentComponent,
+    DashboardComponent,
+    HomeComponent
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [StudentService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
